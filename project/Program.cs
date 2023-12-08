34de7ba5922a56215aef45878895c6b9459f1c83
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 05:34:20 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,22 @@
+﻿// В строке, которую вводит пользователь, определите количество слов,
+// добавьте в начало строки слово Start и в конец строки слово с;
+
+using System;
+
+class work_1_5
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: "); 
+        string user = Console.ReadLine(); 
+
+        int count = 1; 
+        foreach (char sym in user) 
+        { 
+            if (sym == ' ') count++; 
+        } 
+        
+        Console.WriteLine($"Изменённая строка: Start {user} End");
+        Console.WriteLine($"Количество слов в строке: {count}"); 
+    }
+}
